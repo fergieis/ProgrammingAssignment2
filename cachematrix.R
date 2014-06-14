b95acb267b8a4289785669368b068cf367b0f703
fergieis <fergieis@yahoo.com>
2014-06-13 21:54:42 -0400

    Update cachematrix.R
    Added makeCacheMatrix wrapper and cacheSolve function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions provide a wrapper around a matrix object to reduce calls to the "solve" function when
+## computing an inverse matrix.  Inverse is calculated once and then stored in the makeCacheMatrix wrapper
 
-## Write a short comment describing this function
-
+## This function takes a matrix object and constructs a wrapper that stores an inverse matrix once calculated
 makeCacheMatrix <- function(x = matrix()) {
+      
+        ## initialize inverse value 
+        mInv <- NULL
+      
+        ## function to reset value of matrix and inverse
+        set <- function(mm){
+                m <<- mm
+                mInv <<- NULL
+        }## end func set
 
-}
+        ## Function to return original matrix
+        get <- function() m
+      
+        ## function to return inverse matrix
+        getInverse <- function() mInv
+
+        ## function to calculate inverse matrix
+        setInverse <- function(solve) mInv <<- solve
+
+        ## return list of functions
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+
+}## end function makeCacheMatrix
 
 
-## Write a short comment describing this function
-
+## Function returns the inverse matrix of a "makeCacheMatrix" object, either by looking up the value
+## if previous calculated, or calling the "solve" function for the first time
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        
+        m <- x$getInverse()
+        
+        ## check if inverse was previously calculated
+        if(!is.null(m)) {
+                return(m)
+        }
+
+        ## if inverse is needed, then pull matrix and send to solve()
+        data <- x$get()
+        m <- solve(data, ...)
+
+        ## store inverse for later then return it
+        x$setInverse(m)
+        m
+
+}## end function cacheSolve
